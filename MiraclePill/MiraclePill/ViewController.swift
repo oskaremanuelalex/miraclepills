462011600852e236b41de9bbe3978bde5b376c89
Oskar Alex <oskaremanuelalex@gmail.com>
2017-04-27 21:18:26 +0200

    first progress, basic layout and first text field. constraints are working fine.

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Oskar Alex on 2017-04-27.
+//  Copyright © 2017 Oskar Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
